655538279b59a33ea2f19810ba0c4615d0471d0d
“jabberwocky0139” <torii0139@asagi.waseda.jp>
2016-07-07 18:12:32 +0900

    change coordinate to 2D-polar

--- QuantumVortex.py/QV.py
+++ QuantumVortex.py/QV.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 from scipy.integrate import simps
+from scipy.linalg import solve
 from scipy.fftpack import fft
 from scipy.fftpack import ifft
 from scipy.fftpack import fftfreq
@@ -14,69 +15,124 @@ class GrossPitaevskii():
     def __init__(self, gN=50):
 
         # Volume, Maximum x-step
-        self.L, self.xN = 16.0, 1024
+        self.L, self.xN = 8, 256
 
         # Interaction constant, Chemical potential
-        self.gN, self.mu = gN, 10
+        self.gN, self.mu, self.kappa = gN, 10, 2
 
         # Maximum time, Time step 
-        self.tMax, self.tN = 2, 256
+        self.tMax, self.tN = 2, 512
 
         # x-step, Time step
         self.h, self.dt = self.L/self.xN, self.tMax/self.tN
 
         # Set x-space
-        self.x = np.linspace(-self.L/2, self.L/2, self.xN)
+        self.x = np.linspace(0, self.L, self.xN)
 
         # Set time propagator of poptential term except non-linear term
         self.arr_Psi = self.__Psi_0(self.x)
-        self.pre_expV = np.exp((self.mu - self.__V(self.x))*self.dt)
-        self.expK = np.exp(-(2*np.pi*fftfreq(self.xN, d=1/self.xN)/self.L)**2*self.dt)
-        self.expV = np.exp((-self.gN*self.__Psi_0(self.x)**2)*self.dt)*self.pre_expV
+        self.arr_Psi2D = None
 
     
     # --*-- Set functions --*--
 
     # Set initial function
     def __Psi_0(self, x):
-        return x*np.exp(-x**2/2)
+        return x**(self.kappa)*np.exp(-x**2/2)
 
     # Set potential
     def __V(self, x):
-        return 25*np.sin(x/5)**2
+        #return 25*np.sin(x/5)**2
+        return x**2
+    
 
-    # Time evolution
-    def __Symplectic(self):
+    def __MakeCrankMatrix(self):
         
-        for i in range(self.tN):
-            # Time evolution
-            self.arr_Psi = ifft(fft(self.arr_Psi*self.expV)*self.expK)
+        a = np.diag(-2 - self.kappa**2/np.arange(1, self.xN+1)**2 + self.h**2*(self.mu - self.__V(self.x) - self.gN*self.arr_Psi**2 - 2/self.dt))
+                
+        c = np.diag(1 + 0.5/np.arange(0, self.xN))
+        c = np.vstack((c[1:], np.array([0]*self.xN)))
+                
+        d = np.diag(1 - 0.5/np.arange(1, self.xN+1))
+        d = np.vstack((d[1:], np.array([0]*self.xN))).T
+        
+        # Debug on non-interacting system
+        #a = np.diag(-2 + self.h**2*(self.mu - self.__V(self.x) - self.gN*self.arr_Psi**2 - 2/self.dt))
+        #c = np.vstack((np.eye(self.xN)[1:], np.array([0]*self.xN)))
+        #d = c.T
+        
+        return a + c + d
+
+    def __MakeCrankVector(self):
+        
+        a = (-2 - self.kappa**2/np.arange(1, self.xN+1)**2 + self.h**2*(self.mu - self.__V(self.x) - self.gN*self.arr_Psi**2 + 2/self.dt))*self.arr_Psi
+                
+        c = (1 + 0.5/np.arange(1, self.xN+1))*np.hstack((self.arr_Psi[1:], [0]))
+                
+        d = (1 - 0.5/np.arange(1, self.xN+1))*np.hstack(([0], self.arr_Psi[:self.xN-1]))
+
+        # Debug on non-interacting system
+        #a = (-2 + self.h**2*(self.mu - self.__V(self.x) - self.gN*self.arr_Psi**2 + 2/self.dt))*self.arr_Psi
+        #c = np.hstack((self.arr_Psi[1:], [0]))
+        #d = np.hstack(([0], self.arr_Psi[:self.xN-1]))
+        
+        return -a - c - d
+
+    def __GaussSeidel(self):
+        #--- 行列の用意 ---#
+        a = self.__MakeCrankMatrix()
+        #--- ベクトルの用意 ---#
+        b = self.__MakeCrankVector()
+        
+        #--- 連立方程式の計算 ---#
+        self.arr_Psi = solve(a, b)
+        #--- μの補正 ---#
+        norm2 = simps(self.arr_Psi**2, self.x)
+        self.mu -= (norm2 - 1)/(2*self.dt)
+
+        # 規格化 #
+        self.arr_Psi = self.arr_Psi/np.sqrt(norm2)
+
+    def GaussSeidelLoop(self):
+        old_mu = 0
+        while(np.abs(self.mu - old_mu) > 1e-5):
+            old_mu = self.mu
+            self.__GaussSeidel()
+            #self.PrintProcedure()
+            print(self.mu)
+
+    def ExpandFor2D(self):
+        self.arr_Psi2D = [[0 for i in range(self.xN)] for j in range(self.xN)]
+
+        for i in range(self.xN):
+            for j in range(self.xN):
+                index = int(np.sqrt((i - self.xN/2)**2 + (j - self.xN/2)**2))
+                self.arr_Psi2D[i][j] = self.arr_Psi[index]
+        print(self.arr_Psi2D)
             
-            # Correction of chemical potential mu
-            self.mu -= (simps(self.arr_Psi**2, self.x) - 1)/(2*self.dt)
-
-            # Normalization of order parameter arr_Psi
-            self.arr_Psi /= np.sqrt(simps(np.real(self.arr_Psi**2), self.x))
-
-            # Reconfigure expV
-            self.expV = np.exp((-self.gN*self.arr_Psi**2)*self.dt)*self.pre_expV
-            
-
-    def PrintProcedure(self):
-        self.__Symplectic()
+    def PrintProcedureFor1D(self):
 
         # --*-- Matplotlib configuration --*--
         plt.plot(self.x, np.real(self.arr_Psi**2), label="gN = {0}".format(self.gN))
         plt.plot(self.x, self.__V(self.x)/50, label="Potential\n(provisional)")
         plt.ylim(0, 0.5)
+        plt.xlim(0, 6)
         plt.xlabel("q-space")
         plt.ylabel("Order parameter")
         plt.title("Static soliton of Gross-Pitaevskii equation")
         plt.legend(loc = 'center right')
         plt.show()
 
+    def PrintProcedureFor2D(self):
+
+        
+
 
 #GrossPitaevskii.PrintProcedure()
 hundle = GrossPitaevskii(gN=50)
-hundle.PrintProcedure()
+#print(hundle.MakeCrankVector())
+hundle.GaussSeidelLoop()
+#hundle.PrintProcedureFor1D()
+hundle.ExpandFor2D()
+#hundle.PrintProcedure()
 
